a6eea304f09b6ec1e1d99a8ab05647d81e1e8260
Binyuk Dmytrij <dbinyuk@griddynamics.com>
2013-11-15 18:49:26 +0200

    script for backup added

--- 2.2-backup.sh
+++ 2.2-backup.sh
@@ -3,4 +3,16 @@
 #man tar for details
 #script to backup home folder
 
-tar -cvjf /tmp/home-backup.tgz $HOME
+#tar -cvjf /tmp/home-backup.tgz $HOME
+
+#date time output 
+A=$(date +%Y_%e-%B_%H:%M:%S)
+echo $A
+
+#directory path
+P=/home/dbinyuk/work/bash-lrn/
+cat $P $A
+
+
+
+
