9803bb56feac32bbb98d0aa4a526c9eaff7fc11e
Binyuk Dmytrij <dbinyuk@griddynamics.com>
2013-11-15 18:51:03 +0200

    script for backup folder added

---
+++ backup_folder.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+#description: script to create snapshot of a folder
+
+FOLDER=$1
+PREFIX=$2
+
+if [ -z "$FOLDER" ]; then
+	echo "Specify Folder"
+	exit 1
+fi
+
+if [ -z "$PREFIX" ]; then
+	echo "Specify Prefix"
+	exit 2
+fi
+
+if [ ! -d "$FOLDER" ]; then
+	echo "Folder is not found: $FOLDER"
+	exit 3
+fi
+
+
+SUFFIX=".tar.bz"
+BACKUP_FOLDER="/tmp"
+DATE=$(date +%Y%m%d%H%M%S)
+FILENAME="$BACKUP_FOLDER/${PREFIX}_${DATE}${SUFFIX}"
+
+
+tar -cjf $FILENAME $FOLDER
+
+if [ "$?" = "0" ]; then
+	echo "$FILENAME created successfully"
+fi
