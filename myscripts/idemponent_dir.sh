b3aae1b34010aa1270516c1e49b9cf409f4a8e27
Binyuk Dmytrij <dbinyuk@griddynamics.com>
2013-11-26 18:58:42 +0200

    script for adding directory added

---
+++ myscripts/idemponent_dir.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+function make_dir() {
+	local DIR=${1?Directory name required}
+
+	if test -d $DIR; then
+		echo "This directory already exists";
+	else 
+		echo "Directory $DIR created successfully";
+		mkdir $DIR;
+	fi
+}
+
+make_dir "/home/svn/myproject/testo"
