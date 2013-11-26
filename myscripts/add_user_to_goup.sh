43a539e226395cc5f3297c76d0688245f93b194e
Binyuk Dmytrij <dbinyuk@griddynamics.com>
2013-11-26 18:57:29 +0200

    script for add user to group added

---
+++ myscripts/add_user_to_goup.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+function add_user_to_group() {
+if groups www-data | grep  subversion2; then
+	echo "This user is already in the group"
+else
+	echo "User added to group"
+	sudo usermod -a -G subversion dbinyuk
+
+}
