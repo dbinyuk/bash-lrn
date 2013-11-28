284838dc3ca7f6944396e09f8f67280477c52c0a
Binyuk Dmytrij <dbinyuk@griddynamics.com>
2013-11-28 13:41:02 +0200

    script for ariphmetic evaluations added

---
+++ 10.2-arithmetic_evaluations.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+#echo "result of 1 + 1 uotput:"  1 + 1
+#echo "result of \$((1+1)) uotput:" $((1+1))
+#echo "result of \$[1+1] uotput:" $[1+1]
+echo "result of \$[3/4] uotput:" $[3/4]
+echo "result of 3/4|bc -l uotput:" 3/4|bc -l
