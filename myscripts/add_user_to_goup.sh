b158d607c3cb10a29e86d18bbf249c9fcf0f6e82
Binyuk Dmytrij <dbinyuk@griddynamics.com>
2013-12-13 19:00:52 +0200

    numeros resets and reverts are finished

--- myscripts/add_user_to_goup.sh
+++ myscripts/add_user_to_goup.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 function add_user_to_group() {
-if groups www-data | grep  subversion2; then
-	echo "This user is already in the group"
+	USER=${1?Username required}
+	GROUP=${2?Group name required}
+
+if groups $USER | grep $GROUP >/dev/null; then
+	echo "User ${USER} is already in the group: ${GROUP}"
 else
-	echo "User added to group"
-	sudo usermod -a -G subversion dbinyuk
+	echo "User ${USER} added to group: ${GROUP}"
+	sudo usermod -a -G ${GROUP} ${USER}
 fi
 }
 
-add_user_to_group
+
+add_user_to_group "www-data" "subversion"
 
