52cbf14bd90a9fd2690551f2af95b5b93a0d10e3
Binyuk Dmytrij <dbinyuk@griddynamics.com>
2014-01-28 18:15:36 +0200

    chef client install on local machine cache options and dir for checksum var/chef

--- myscripts/chef/chef_client_install_local.sh
+++ myscripts/chef/chef_client_install_local.sh
@@ -1,11 +1,11 @@
 #!/bin/bash -evx
 
-#description
-#author
-#date
+# author: Dmitry Binyuk <dbinyuk@griddynamics.com>
+# description: script for chef client install on local machine
+# created: 24.01.2014 10:56
+# last changed: 24.01.2014 17:33
 
 CHEF_CLIENT_NAME=${1?"Specify CHEF_CLIENT_NAME to create"}
-#local account
 CHEF_ADMIN_CLIENT_NAME=${2?"Specify CHEF_ADMIN_CLIENT_NAME"}
 SERVER_ADDR=${3?"Specify SERVER_ADDR like 173.1.205.67"}
 
@@ -16,9 +16,10 @@ CHEF_PATH=$CHEFUSER_HOME/.chef
 KNIFEFILE=$CHEF_PATH/knife.rb
 
 #mkdir ~/.chef
-
-ssh $SERVER_ADDR "sudo su $CHEF_ADMIN_CLIENT_NAME -c 'knife client create $CHEF_CLIENT_NAME -d -a'" > ~/.chef/$CHEF_CLIENT_NAME.pem
-ssh $SERVER_ADDR "sudo su $CHEF_ADMIN_CLIENT_NAME -c 'cat ~/.chef/validation.pem'" > ~/.chef/validation.pem
+sudo mkdir -p /var/chef
+sudo chmod 777 /var/chef
+#ssh $SERVER_ADDR "sudo su $CHEF_ADMIN_CLIENT_NAME -c 'knife client create $CHEF_CLIENT_NAME -d -a'" > $CHEFUSER_HOME/$CHEF_CLIENT_NAME.pem
+ssh $SERVER_ADDR "sudo su $CHEF_ADMIN_CLIENT_NAME -c 'cat ~/.chef/validation.pem'" > $CHEFUSER_HOME/validation.pem
 
 cat <<- EOH > $KNIFEFILE
 log_level                :info
@@ -32,7 +33,9 @@ validation_key           '$CHEF_PATH/validation.pem'
 
 chef_server_url          'http://$SERVER_ADDR:4000'
 syntax_check_cache_path  '$CHEF_PATH/syntax_check_cache'
+cookbook_path [ "$CHEFUSER_HOME/chef-repo/cookbooks" ]
+cache_options({ :path => "/var/chef/cache/checksums", :skip_expires => true })
+
 EOH
 
 knife client list | grep "$CHEF_CLIENT_NAME" && echo SUCCESS
-
